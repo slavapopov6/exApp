9667fb5507e18732e2964e499f6436d50acc1837
Slava <veaceslavpopov6@gmail.com>
2020-08-09 14:20:08 +0300

    escape

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,45 @@
-s;
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extentions: [".js"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@core": path.resolve(__dirname, "src/core"),
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: "index.html",
+    }),
+    new CopykPlugin([
+      {
+        from: path.resolve(__dirname, "src/favicon.ico"),
+        to: path.resolve(__dirname, "dist"),
+      },
+    ]),
+    new MiniCssPlugin({
+      filename: "bundle.[hash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
